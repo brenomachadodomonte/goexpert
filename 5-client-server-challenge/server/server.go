4ea8d4add2a0069d956d4f605308232b929aebf4
brenomachadodomonte <brenomachadodomonte@gmail.com>
2023-04-30 19:47:05 -0300

    Creating get all quotations endpoint

--- 5-client-server-challenge/server/server.go
+++ 5-client-server-challenge/server/server.go
@@ -37,6 +37,12 @@ type JsonError struct {
 	Message string `json:"message"`
 }
 
+type QuotationModel struct {
+	ID    int     `json:"id"`
+	Value float64 `json:"value"`
+	Date  string  `json:"date"`
+}
+
 func main() {
 	db, err := sql.Open("sqlite3", "db.sqlite")
 	if err != nil {
@@ -49,8 +55,12 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		GetQuotationsHandler(db, writer)
+	})
+
 	mux.HandleFunc("/cotacao", func(writer http.ResponseWriter, request *http.Request) {
-		QuotationHandler(db, writer, request)
+		QuotationHandler(db, writer)
 	})
 
 	err = http.ListenAndServe(":8080", mux)
@@ -59,7 +69,7 @@ func main() {
 	}
 }
 
-func QuotationHandler(db *sql.DB, writer http.ResponseWriter, request *http.Request) {
+func QuotationHandler(db *sql.DB, writer http.ResponseWriter) {
 	writer.Header().Set("Content-Type", "application/json")
 	quotation, err := GetQuotation()
 	if err != nil {
@@ -83,8 +93,21 @@ func QuotationHandler(db *sql.DB, writer http.ResponseWriter, request *http.Requ
 	json.NewEncoder(writer).Encode(resp)
 }
 
+func GetQuotationsHandler(db *sql.DB, writer http.ResponseWriter) {
+	writer.Header().Set("Content-Type", "application/json")
+
+	quotations, err := getAllQuotations(db)
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(writer).Encode(&JsonError{Message: err.Error()})
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(quotations)
+}
+
 func saveQuotation(db *sql.DB, value float64) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
 	defer cancel()
 	currentTime := time.Now().Format("2006-01-02 15:04:05.000000")
 	stmt, err := db.Prepare("insert into quotations(value, date) values (?, ?)")
@@ -97,6 +120,26 @@ func saveQuotation(db *sql.DB, value float64) error {
 		return err
 	}
 	return nil
+}
+
+func getAllQuotations(db *sql.DB) ([]QuotationModel, error) {
+	rows, err := db.Query("select id, value, date from quotations")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var quotations []QuotationModel
+	for rows.Next() {
+		var quotation QuotationModel
+		err = rows.Scan(&quotation.ID, &quotation.Value, &quotation.Date)
+		if err != nil {
+			return nil, err
+		}
+		quotations = append(quotations, quotation)
+	}
+
+	return quotations, nil
 }
 
 func createTable(db *sql.DB) error {
@@ -115,7 +158,7 @@ func createTable(db *sql.DB) error {
 }
 
 func GetQuotation() (*Quotation, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Millisecond)
 	defer cancel()
 
 	url := "https://economia.awesomeapi.com.br/json/last/USD-BRL"
