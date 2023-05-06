0ca13f74c9780f14e0e2517098201c51fb3e30e7
brenomachadodomonte <brenomachadodomonte@gmail.com>
2023-05-06 17:04:10 -0300

    Mocks for tests in go

---
+++ 7-tests/3/tax_test.go
@@ -0,0 +1,43 @@
+package tax
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+type TaxRepositoryMock struct {
+	mock.Mock
+}
+
+func (m *TaxRepositoryMock) SaveTax(tax float64) error {
+	args := m.Called(tax)
+	return args.Error(0)
+}
+
+func TestCalculateTaxAndSave(t *testing.T) {
+	repository := &TaxRepositoryMock{}
+	repository.On("SaveTax", 10.0).Return(nil)
+	repository.On("SaveTax", 0.0).Return(errors.New("error saving tax"))
+
+	err := CalculateTaxAndSave(1000.0, repository)
+	assert.Nil(t, err)
+
+	err = CalculateTaxAndSave(0.0, repository)
+	assert.Error(t, err, "error saving tax")
+
+	repository.AssertExpectations(t)
+	repository.AssertNumberOfCalls(t, "SaveTax", 2)
+}
+
+func TestCalculateTax(t *testing.T) {
+	amount := 500.0
+	expected := 5.0
+
+	result := CalculateTax(amount)
+
+	if result != expected {
+		t.Errorf("Expected %f but got %f", expected, result)
+	}
+}
