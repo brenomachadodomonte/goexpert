b6d83564a16bb6ee7633b4fcb7d8cf4ba435b60a
brenomachadodomonte <brenomachadodomonte@gmail.com>
2023-06-12 21:48:08 -0300

    Solving Concurrency problem with Lock and UnLock

--- 9-multithreading/3-real-world-problem/main.go
+++ 9-multithreading/3-real-world-problem/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 var number uint64 = 0
 
 func main() {
+	m := sync.Mutex{}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		m.Lock()
 		number++
+		m.Unlock()
 		_, err := writer.Write([]byte(fmt.Sprintf("You accessed this page %d times", number)))
 		if err != nil {
 			writer.WriteHeader(http.StatusInternalServerError)
