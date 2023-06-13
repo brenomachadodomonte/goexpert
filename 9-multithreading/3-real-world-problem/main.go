b8af6a4aacbebfaa5b530d61e8c25ea7051f64a3
brenomachadodomonte <brenomachadodomonte@gmail.com>
2023-06-12 21:53:41 -0300

    Solving Concurrency problem with atomic

--- 9-multithreading/3-real-world-problem/main.go
+++ 9-multithreading/3-real-world-problem/main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
+	"sync/atomic"
 )
 
 var number uint64 = 0
 
+// go run -race main.go to check if is there a race condition
 func main() {
-	m := sync.Mutex{}
+	//m := sync.Mutex{}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		m.Lock()
-		number++
-		m.Unlock()
+		//m.Lock()
+		atomic.AddUint64(&number, 1)
+		//m.Unlock()
 		_, err := writer.Write([]byte(fmt.Sprintf("You accessed this page %d times", number)))
 		if err != nil {
 			writer.WriteHeader(http.StatusInternalServerError)
