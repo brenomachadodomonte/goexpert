d34c7115554e1e4e4abfd3324aca6c8c4d0eb1d1
brenomachadodomonte <brenomachadodomonte@gmail.com>
2023-06-12 21:12:04 -0300

    Go Routines

---
+++ 9-multithreading/1-go-routines/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task(name string) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d: Task %s is running\n", i, name)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// Thread 1
+func main() {
+	// Thread 2
+	go task("A")
+	// Thread 3
+	go task("B")
+
+	time.Sleep(15 * time.Second)
+}
