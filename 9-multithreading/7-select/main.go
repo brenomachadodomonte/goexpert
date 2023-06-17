8bb161e5742b1aab09453280d0a9fc121d241b20
brenomachadodomonte <brenomachadodomonte@gmail.com>
2023-06-17 15:13:11 -0300

    Select operator in go

--- 9-multithreading/7-select/main.go
+++ 9-multithreading/7-select/main.go
@@ -1,27 +1,43 @@
 package main
 
-import "time"
+import (
+	"sync/atomic"
+	"time"
+)
+
+type Message struct {
+	ID  int64
+	Msg string
+}
 
 func main() {
-	c1 := make(chan int)
-	c2 := make(chan int)
-
+	c1 := make(chan Message)
+	c2 := make(chan Message)
+	var i int64 = 0
 	go func() {
-		c1 <- 1
-		time.Sleep(time.Second * 1)
+		atomic.AddInt64(&i, 1)
+		for {
+			time.Sleep(time.Second * 1)
+			msg := Message{ID: i, Msg: "Hello from RabbitMQ"}
+			c1 <- msg
+		}
 	}()
 
 	go func() {
-		time.Sleep(time.Second * 1)
-		c2 <- 2
+		atomic.AddInt64(&i, 1)
+		for {
+			time.Sleep(time.Second * 2)
+			msg := Message{ID: i, Msg: "Hello from Kafka"}
+			c2 <- msg
+		}
 	}()
 
-	for i := 0; i < 3; i++ {
+	for {
 		select {
 		case msg1 := <-c1:
-			println("Received", msg1)
+			println("Received", msg1.Msg, "ID", msg1.ID)
 		case msg2 := <-c2:
-			println("Received", msg2)
+			println("Received", msg2.Msg, "ID", msg2.ID)
 		case <-time.After(time.Second * 3):
 			println("Timout")
 			//default:
