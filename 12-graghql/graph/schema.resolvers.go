4d4a3de4c8387b09faee6dbda9bdd84e50819dc8
brenomachadodomonte <brenomachadodomonte@gmail.com>
2023-07-22 20:06:19 -0300

    Creating create category query

--- 12-graghql/graph/schema.resolvers.go
+++ 12-graghql/graph/schema.resolvers.go
@@ -32,7 +32,20 @@ func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCour
 
 // Categories is the resolver for the categories field.
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
-	panic(fmt.Errorf("not implemented: Categories - categories"))
+	categories, err := r.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var modelCategories []*model.Category
+	for _, c := range categories {
+		modelCategories = append(modelCategories, &model.Category{
+			ID:          c.ID,
+			Name:        c.Name,
+			Description: &c.Description,
+		})
+	}
+	return modelCategories, nil
 }
 
 // Courses is the resolver for the courses field.
