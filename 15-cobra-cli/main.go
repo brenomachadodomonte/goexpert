177968f883c258b504a1d32982195699bad82a9e
brenomachadodomonte <brenomachadodomonte@gmail.com>
2023-08-26 22:31:10 -0300

    Nested commands

--- 15-cobra-cli/main.go
+++ 15-cobra-cli/main.go
@@ -3,7 +3,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "go-expert/15-cobra-cli/cmd"
+import "github.com/brenomachadodomonte/go-expert/cobra/cmd"
 
 func main() {
 	cmd.Execute()
